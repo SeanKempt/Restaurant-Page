0562018f43971f98d209ac4923f343d4907876a3
Sean Kempt <kempt101@gmail.com>
2022-08-17 14:04:40 -0500

    feat: Add aboutus page and function to load page from navigation
    Added aboutus function module that gets imported into Index.js
    
    Add event listener for the aboutus navigation button
    
    Add about us page content and altered styles so they are consistent with site design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { intializePage } from "./modules/pageload.js";
 import { homeMainContent } from "./modules/home.js";
 import { createMenu } from "./modules/menu";
+import { createAboutUs } from "./modules/about.js";
 
 intializePage();
 homeMainContent();
@@ -16,6 +17,12 @@ const homeNav = document.getElementById("home-nav");
 homeNav.addEventListener("click", function () {
   clearPage();
   homeMainContent();
+});
+
+const aboutNav = document.getElementById("about-us-nav");
+aboutNav.addEventListener("click", function () {
+  clearPage();
+  createAboutUs();
 });
 
 const clearPage = () => {
--- src/modules/about.js
+++ src/modules/about.js
@@ -1,1 +1,25 @@
-//create
+import { createParagraph } from "./pageload";
+
+function createAboutUs() {
+  const main = document.getElementById("main");
+  const aboutUsTitle = document.createElement("h1");
+  const aboutUsContent = document.createElement("div");
+
+  aboutUsContent.id = "about-us-content";
+
+  aboutUsTitle.innerHTML = `About Rocket's Cantina <hr>`;
+  aboutUsContent.appendChild(
+    createParagraph(
+      `Established in 1976, Rocket's cantina was built by its orignal owner Johnny "Rocket" Doe. Rocket had a vision for the cantina to be the preferred 
+      Mexican resturaunt in all of town and have the highest standard of quality. Today Rocket's Cantina still stands and is the towns number one go to for Mexican cuisine.
+      The team at Rocket's Cantina is proud to continue to serve the town of nowhere providing an authentic Mexican food experience for years to come.`
+    )
+  );
+
+  main.appendChild(aboutUsTitle);
+  main.appendChild(aboutUsContent);
+
+  return main;
+}
+
+export { createAboutUs };
